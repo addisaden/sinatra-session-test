4fee449105c015ec9a74b60007f39f32152c2c74
Adrian E <ae@writedown.eu>
2013-03-07 12:19:16 +0100

    test completed

---
+++ session_test.rb
@@ -0,0 +1,20 @@
+﻿#encoding: utf-8
+
+require "sinatra/base"
+require "slim"
+
+module SessionTest
+  class SessionTest < Sinatra::Base
+    # enable :sessions
+    use Rack::Session::Pool, :expire_after => 86400
+    set :views, File.expand_path("../views", __FILE__)
+
+    get "/" do
+      session[:value] ||= rand(1000)
+      @session_id = session.inspect
+      slim :session, layout: true
+    end
+  end
+end
+
+SessionTest::SessionTest.run!
